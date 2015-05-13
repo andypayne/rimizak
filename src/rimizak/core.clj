7d204e977f4811fb90f3c4053a1c9570228fc6cc
andpayne <andpayne@cisco.com>
2015-05-12 20:25:57 -0700

    Refactor icosahedron indices

--- src/rimizak/core.clj
+++ src/rimizak/core.clj
@@ -180,32 +180,34 @@ endsolid Rimizak_Model\n"))
 		]))
 
 (def icosahedron-facets
-	(let [ivs icosahedron-vertices]
-		[
-			[(nth ivs 0)  (nth ivs 11) (nth ivs 5)]
-			[(nth ivs 0)  (nth ivs 5)  (nth ivs 1)]
-			[(nth ivs 0)  (nth ivs 1)  (nth ivs 7)]
-			[(nth ivs 0)  (nth ivs 7)  (nth ivs 10)]
-			[(nth ivs 0)  (nth ivs 10) (nth ivs 11)]
+	(let [ivs icosahedron-vertices
+				indices [
+					[ 0 11  5]
+					[ 0  5  1]
+					[ 0  1  7]
+					[ 0  7 10]
+					[ 0 10 11]
 
-			[(nth ivs 1)  (nth ivs 5)  (nth ivs 9)]
-			[(nth ivs 5)  (nth ivs 11) (nth ivs 4)]
-			[(nth ivs 11) (nth ivs 10) (nth ivs 2)]
-			[(nth ivs 10) (nth ivs 7)  (nth ivs 6)]
-			[(nth ivs 7)  (nth ivs 1)  (nth ivs 8)]
+					[ 1  5  9]
+					[ 5 11  4]
+					[11 10  2]
+					[10  7  6]
+					[ 7  1  8]
 
-			[(nth ivs 3)  (nth ivs 9)  (nth ivs 4)]
-			[(nth ivs 3)  (nth ivs 4)  (nth ivs 2)]
-			[(nth ivs 3)  (nth ivs 2)  (nth ivs 6)]
-			[(nth ivs 3)  (nth ivs 6)  (nth ivs 8)]
-			[(nth ivs 3)  (nth ivs 8)  (nth ivs 9)]
+					[ 3  9  4]
+					[ 3  4  2]
+					[ 3  2  6]
+					[ 3  6  8]
+					[ 3  8  9]
 
-			[(nth ivs 4)  (nth ivs 9)  (nth ivs 5)]
-			[(nth ivs 2)  (nth ivs 4)  (nth ivs 11)]
-			[(nth ivs 6)  (nth ivs 2)  (nth ivs 10)]
-			[(nth ivs 8)  (nth ivs 6)  (nth ivs 7)]
-			[(nth ivs 9)  (nth ivs 8)  (nth ivs 1)]
-		]))
+					[ 4  9  5]
+					[ 2  4 11]
+					[ 6  2 10]
+					[ 8  6  7]
+					[ 9  8  1]
+				]
+		]
+		(map (fn [[x y z]] [(nth ivs x) (nth ivs y) (nth ivs z)]) indices)))
 
 (defn icosahedron-solid []
 	(map (fn [facet]
